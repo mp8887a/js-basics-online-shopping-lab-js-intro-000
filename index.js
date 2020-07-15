65aa68248584a273e21c00a6b9df0c8329946c02
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-15 15:40:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,21 +26,25 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length === 0) {
-    return `Your shopping cart is empty.`
+ // write your code here
+  if (cart.length === 0){
+    console.log('Your shopping cart is empty.')
+  } else if (cart.length === 1){
+    var oneItem = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`
+    console.log(oneItem)
+  } else if (cart.length === 2){
+    var twoItems = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`
+    console.log(twoItems)
   } else {
-    var cartItems = []
-
-    var last = `${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
-
-    for(var i = 0; i < cart.length-1; i++){
-       cartItems.push(
-         `${cart[i].itemName} at $${cart[i].itemPrice}`
-       )
-    }
-    return `In your cart, you have ${cartItems.join(', ')}, and ${last}`
-  }
+    var cartData = []
+    for (var i = 0; i < cart.length-1; i++){
+      cartData.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
 }
+    var threePlusItems = `In your cart, you have ${cartData.join(', ')}, and ${Object.keys(cart[cart.length-1])} at $${Object.values(cart[cart.length-1])}.`
+  }
+  console.log(threePlusItems)
+}
+viewCart()
 
 
 function total(){
