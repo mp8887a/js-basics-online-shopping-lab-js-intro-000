28183f2f9a1aa541c339b05f0ad216e621e01880
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-15 16:17:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,11 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     return `Your shopping cart is empty.`
-  } else {
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+  }
+
+  else {
     var cartItems = []
 
     var last = `${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
